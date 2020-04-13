abc7fdb38e5c530192dd54d821403bd62a84fe8d
wildtigon <wildtigon@gmail.com>
2020-04-13 11:46:15 +0700

    update configs

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DNDownloader",
-            path: "Sources",
+            path: "Sources/DNDownloader",
             dependencies: []),
         .testTarget(
             name: "DNDownloaderTests",
