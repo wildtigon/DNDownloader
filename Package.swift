a80411aa8983b23f28b181c285262172b2a00790
wildtigon <wildtigon@gmail.com>
2020-04-13 11:40:53 +0700

    exclude unnecessary files

--- Package.swift
+++ Package.swift
@@ -23,6 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DNDownloader",
+            path: "Sources",
             dependencies: []),
         .testTarget(
             name: "DNDownloaderTests",
