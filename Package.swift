e129e50155f062a01c1204bfd189ad480de687a3
wildtigon <wildtigon@gmail.com>
2020-04-13 11:04:40 +0700

    update configs

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DNDownloader",
-            targets: ["DNDownloader"]),
             path: "./DNDownloader",
+            targets: ["DNDownloader"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,8 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DNDownloader",
-            dependencies: []),
             path: "./DNDownloader",
+            dependencies: []),
         .testTarget(
             name: "DNDownloaderTests",
             dependencies: ["DNDownloader"]),
